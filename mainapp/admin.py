197cdfb630db29660fa7be3cb40b4787aadad8bb
Philipp Kachanov <fill2907@gmail.com>
2021-01-02 17:56:27 +0300

    -Зарегестрировал модели в админке. -Написал класс для соответствующего отображения категорий.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,41 @@
+from django import forms
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class NotebookCategoryChoiceFiled(forms.ModelChoiceField):
+
+    pass
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # При добавление ноутбука в категориях выводим только категорию ноутбуки
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceFiled(Category.objects.filter(slug="notebooks"))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneCategoryChoiceFiled(forms.ModelChoiceField):
+
+    pass
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # При добавление смартфона в категориях выводим только категорию смартфоны
+        if db_field.name == 'category':
+            return SmartphoneCategoryChoiceFiled(Category.objects.filter(slug="smartphones"))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(Customer)
--- mainapp/models.py
+++ mainapp/models.py
@@ -20,12 +20,12 @@ class Product(models.Model):
     class Meta:
         abstract = True
 
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name="Наименование")
     slug = models.SlugField(unique=True)
     image = models.ImageField(verbose_name="Изображение")
     description = models.TextField(verbose_name="Описание", null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
-    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
