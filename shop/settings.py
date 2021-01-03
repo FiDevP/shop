e9cecab9780eefd2ef574663d2aa7ce271c64eec
Philipp Kachanov <fill2907@gmail.com>
2021-01-03 13:51:39 +0300

    Добавил возможность сортировки по продукту

--- shop/settings.py
+++ shop/settings.py
@@ -127,6 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev')
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static_dev')
+# )
