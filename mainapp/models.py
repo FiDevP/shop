71fb02bef64d055d9d7784edbc4407f62776c812
Philipp Kachanov <fill2907@gmail.com>
2021-01-06 16:40:46 +0300

    -новая модель Orders -На странице категории отображаются продукты конкретной категории -в корзине добавлены алерты -в корзине можно менять количество товаров -можно добавлять продукты в корзину с каждой странице

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.urls import reverse
+from django.utils import timezone
 
 from PIL import Image
 
@@ -133,6 +134,9 @@ class Product(models.Model):
         )
         super().save(*args, **kwargs)
 
+    def get_model_name(self):
+        return self.__class__.__name__.lower()
+
 
 class Notebook(Product):
 
@@ -168,12 +172,6 @@ class Smartphone(Product):
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
 
-    #@property
-    #def sd(self):
-    #    if self.sd:
-    #        return 'Да'
-    #    return 'Нет'
-
 
 class CartProduct(models.Model):
 
@@ -188,25 +186,83 @@ class CartProduct(models.Model):
     def __str__(self):
         return f"Продукт: {self.content_object.title}(для корзины)"
 
+    def save(self, *args, **kwargs):
+        self.final_price = self.qty * self.content_object.price
+        super().save(*args, **kwargs)
+
 
 class Cart(models.Model):
 
-    owner = models.ForeignKey('Customer', verbose_name="Владелец", on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer', verbose_name="Владелец", null=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name="related_cart")
     total_products = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, default=0, verbose_name="Общая цена")
     in_order = models.BooleanField(default=False)
     for_anonymous_user = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.id)
 
+    def save(self, *args, **kwargs):
+        cart_data = self.products.aggregate(models.Sum('final_price'), models.Count('id'))
+        print(cart_data)
+        if cart_data.get('final_price__sum'):
+            self.final_price = cart_data['final_price__sum']
+        else:
+            self.final_price = 0
+        self.total_products = cart_data['id__count']
+        super().save(*args, **kwargs)
+
 
 class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
-    address = models.CharField(max_length=255, verbose_name="Адрес")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона", null=True, blank=True)
+    address = models.CharField(max_length=255, verbose_name="Адрес", null=True, blank=True)
+    orders = models.ManyToManyField('Orders', verbose_name="Заказы покупателя", related_name="related_customer")
 
     def __str__(self):
         return f"Покупатель:{self.user.first_name} {self.user.last_name}"
+
+
+class Orders(models.Model):
+
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'is_ready'
+    STATUS_COMPLETED = 'completed'
+
+    BUYING_TYPE_SELF = 'self'
+    BUYING_TYPE_DELIVERY = 'delivery'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый заказ'),
+        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+        (STATUS_READY, 'Заказ готов'),
+        (STATUS_COMPLETED, 'Заказ выполнен')
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Самовывоз'),
+        (BUYING_TYPE_DELIVERY, 'Доставка')
+    )
+
+    customer = models.ForeignKey(
+        Customer, verbose_name="Покупатель", related_name='related_orders', on_delete=models.CASCADE
+    )
+    first_name = models.CharField(max_length=255, verbose_name="Имя")
+    last_name = models.CharField(max_length=255, verbose_name="Фамилия")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    address = models.CharField(max_length=1024, verbose_name="Адрес", null=True, blank=True)
+    status = models.CharField(
+        max_length=100, verbose_name="Статус заказа", choices=STATUS_CHOICES, default=STATUS_NEW
+    )
+    buying_type = models.CharField(
+        max_length=100, verbose_name="Тип заказа", choices=BUYING_TYPE_CHOICES, default=BUYING_TYPE_SELF
+    )
+    comment = models.TextField(verbose_name="Комментарий к заказу", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Дата создания заказа")
+    order_date = models.DateField(verbose_name="Дата получения заказа", default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
+from django.http import HttpResponseRedirect
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
 
-from .models import Notebook, Smartphone, Category
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
+from .mixins import CategoryDetailMixin, CartMixin
 
 
-def test_views(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+class BaseView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_product_for_main_page('notebook', 'smartphone', with_respect_to='smartphone')
+        context = {
+            'categories': categories,
+            'products': products,
+            'cart': self.cart
+        }
+        return render(request, 'base.html', context)
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -25,11 +37,85 @@ class ProductDetailView(DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        context['cart'] = self.cart
+        return context
 
-class CategoryDetailView(DetailView):
+
+class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart'] = self.cart
+        return context
+
+
+class AddToCartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')  # берем нужные значение из кваргсов
+        content_type = ContentType.objects.get(model=ct_model)  # через контенттайп определяем что за модель должна быть для нашего товара
+        product = content_type.model_class().objects.get(slug=product_slug)  # получаем продукт, вызвая его родительскую модель
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )  # создаем карт продукта
+        if created:
+            self.cart.products.add(cart_product)  # добавляем карт продукта в корзину
+        self.cart.save()
+        messages.add_message(request, messages.INFO, 'Товар успешно добавлен')
+        return HttpResponseRedirect('/cart/')
+
+
+class DeleteFromCartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')  # берем нужные значение из кваргсов
+        content_type = ContentType.objects.get(model=ct_model)  # через контенттайп определяем что за модель должна быть для нашего товара
+        product = content_type.model_class().objects.get(slug=product_slug)  # получаем продукт, вызвая его родительскую модель
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        self.cart.products.remove(cart_product)  # добавляем карт продукта в корзину
+        cart_product.delete()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, 'Товар успешно удален')
+        return HttpResponseRedirect('/cart/')
+
+
+class ChangeQTYView(CartMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')  # берем нужные значение из кваргсов
+        content_type = ContentType.objects.get(
+            model=ct_model)  # через контенттайп определяем что за модель должна быть для нашего товара
+        product = content_type.model_class().objects.get(
+            slug=product_slug)  # получаем продукт, вызвая его родительскую модель
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        qty = int(request.POST.get('qty'))
+        cart_product.qty = qty
+        cart_product.save()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, 'Кол-во успешно изменено')
+        return HttpResponseRedirect('/cart/')
+    
+
+class CartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'categories': categories,
+            'cart': self.cart
+        }
+        return render(request, 'cart.html', context)
